0649600bf72da6ad27837d52905b18d14a494014
Arnaud <arnaud.emprin@gmail.com>
2020-11-05 14:47:48 +0100

    google auth working with redirection

--- emotion-picker/server/index.js
+++ emotion-picker/server/index.js
@@ -19,11 +19,11 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
-/*fs.readFile('credentials.json', (err, content) => {
+fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
   authorize(JSON.parse(content), listFiles);
-});*/
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
