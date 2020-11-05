d1b254b4c78a43c6c6e618a38d81acaf93214631
Arnaud <arnaud.emprin@gmail.com>
2020-11-05 13:04:47 +0100

    fetching pictures from wikiart most viewed paintings

--- emotion-picker/src/App.js
+++ emotion-picker/src/App.js
@@ -2,8 +2,28 @@ import './App.css';
 import React from "react";
 import ImageContainer from './Components/ImageContainer'
 import VotingButtons from './Components/VotingButtons'
+import Button from "@material-ui/core/Button"
 
 class App extends React.Component {
+
+  state = {
+    mostViewedPaintings: [],
+    currentPhoto: 0
+  }
+
+  componentDidMount() {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/', targetUrl = 'https://www.wikiart.org/en/api/2/MostViewedPaintings?imageFormat=Blog'
+    fetch(proxyUrl + targetUrl)
+      .then(res => res.json())
+      .then((response) => {
+        console.log(response)
+        var MostViewedPaintings = response.data
+        this.setState({
+          mostViewedPaintings: MostViewedPaintings
+        })
+      })
+      .catch(error => console.log(error))
+  }
 
   handleVote = () => {
     fetch('/listFiles')
@@ -13,19 +33,41 @@ class App extends React.Component {
       })
   }
 
+  nextPhoto = () => {
+    var currentPhoto = this.state.currentPhoto
+    this.setState({
+      currentPhoto: currentPhoto + 1
+    })
+  }
+
+  previousPhoto = () => {
+    var currentPhoto = this.state.currentPhoto
+    this.setState({
+      currentPhoto: currentPhoto - 1
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Emotion Picker by Neurogram
-          <ImageContainer
-            url='https://images-na.ssl-images-amazon.com/images/I/91I3JlbFABL._AC_SL1500_.jpg'
-            width={400}
-            height={300}
-          />
-          <VotingButtons 
-            callbackClick={this.handleVote}
-          />
+          {this.state.mostViewedPaintings.length !== 0 &&
+            <React.Fragment>
+              <ImageContainer
+                url={this.state.mostViewedPaintings[this.state.currentPhoto].image}
+              //width={400}
+              //height={300}
+              />
+              <div className="d-flex inline">
+                <Button className="navBtn" disabled={this.state.currentPhoto === 0} variant="contained" onClick={this.previousPhoto} value="<">{"<"}</Button>
+                <Button className="navBtn" disabled={this.state.currentPhoto === this.state.mostViewedPaintings.length - 1} variant="contained" onClick={this.nextPhoto} value=">">{">"}</Button>
+              </div>
+              <VotingButtons
+                callbackClick={this.handleVote}
+              />
+            </React.Fragment>
+          }
         </header>
       </div>
     )
--- emotion-picker/src/Components/ImageContainer.js
+++ emotion-picker/src/Components/ImageContainer.js
@@ -4,11 +4,13 @@ import React from "react";
 class ImageContainer extends React.Component {
     render() {
         return (
-            <img
-                src={this.props.url}
-                width={this.props.width}
-                height={this.props.height} 
-            />
+            <div id="currentPainting">
+                <img
+                    src={this.props.url}
+                    width={this.props.width}
+                    height={this.props.height}
+                />
+            </div>
         )
     }
 }
